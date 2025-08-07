4e28bf1edf6d107384fdcd1ad5932c6209f22c0b
Priyavardhanbezawada <priyavardhan.bezawada@gmail.com>
2025-08-07 19:19:17 +0530

    Update resource_finder.py

--- resource_finder.py
+++ resource_finder.py
@@ -8,7 +8,6 @@ import sys
 load_dotenv()
 
 YOUTUBE_API_KEY = os.getenv("YOUTUBE_API_KEY")
-Google Search_API_KEY = os.getenv("Google Search_API_KEY")
 SEARCH_ENGINE_ID = os.getenv("SEARCH_ENGINE_ID")
 
 def _check_api_keys():
@@ -66,3 +65,4 @@ def fetch_all_resources(topic: str) -> list:
     videos = find_youtube_videos(topic)
     articles = find_articles(topic)
     return videos + articles
+
